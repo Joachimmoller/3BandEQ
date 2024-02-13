4d806e2e32022c74082d5a076094e1114a394ed4
Joachimmoller <joachimmoller@gmail.com>
2024-02-13 15:45:05 +0100

    Refactored peakcoefficent generation

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -210,18 +210,23 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
     return settings;
 }
 
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,chainSettings.peakFreq,
+                                                        chainSettings.peakQuality,
+                                                        juce::Decibels::decibelsToGain(
+                                                                chainSettings.peakGainInDecibels));
+}
+
 void AudioPluginAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings){
-    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(),
-                                                                                chainSettings.peakFreq,
-                                                                                chainSettings.peakQuality,
-                                                                                juce::Decibels::decibelsToGain(
-                                                                                        chainSettings.peakGainInDecibels));
+    
+    auto peakCoefficients = makePeakFilter(chainSettings,getSampleRate());
 
     updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
     updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
 
-void AudioPluginAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients &replacements) 
+void /*AudioPluginAudioProcessor::*/updateCoefficients(Coefficients &old, const Coefficients &replacements) 
 {
     *old = *replacements;
 }
